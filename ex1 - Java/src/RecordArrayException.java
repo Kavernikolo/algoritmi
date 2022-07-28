a59774f9f96a1f6342cd9f8ae14f68a895d8383f
Marco <marco.zotti@outlook.com>
2022-07-28 20:39:09 +0200

    reset

--- ex1 - Java/src/RecordArrayException.java
+++
@@ -1,4 +0,0 @@
-public class RecordArrayException extends Exception
-{ 
-    public RecordArrayException(String message){ super(message); } 
-}
--- ex4 - Java/src/GraphException.java
+++
@@ -1,4 +0,0 @@
-public class GraphException extends Exception 
-{
-	public GraphException(String message){ super(message); }
-}
