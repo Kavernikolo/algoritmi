e4864d2fc306771b6c7fb644e7474846dcef868d
Marco <marco.zotti@outlook.com>
2022-08-04 17:26:44 +0200

    First skipList for test function

--- ex2 - Java/src/Interface.java
+++
@@ -1,9 +0,0 @@
-interface Interface<T extends Comparable<? super T>> 
-{
-    int LEVELS = 5;
-  
-    boolean delete(T target);
-    void print();
-    void insert(T data);
-    SkipNode<T> search(T data);
-  }
