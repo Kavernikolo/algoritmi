200a2a58698e425f62424e5f3c03c54d82306d2d
Marco <marco.zotti@outlook.com>
2022-08-04 16:33:16 +0200

    Heap Java finish

--- ex3 - Java/src/Heap.java
+++ ex3 - Java/src/Heap.java
@@ -2,87 +2,67 @@ import java.util.ArrayList;
 
 public class Heap<T extends Comparable<T>>
 {
-    private static final int ROOT = 0;
+    private static final int ROOT = 1;
 
     private ArrayList<T> array;
 
     //Creazione di uno heap minimo vuoto - O(1)
-    public Heap(int size){ array = new ArrayList<>(size); }
+    public Heap(){ array = new ArrayList<>(); array.add(null); }
 
     public void buildMinHeap()
     {
-        for(int i = (array.size() / 2); i >= 0; i--){ heapify(array.size()); }
-
+        //for(int i = (array.size() / 2); i >= 0; i--){ heapify(array.size()); }
         print();
-    }
-
-    private void heapify(int index)
-    {
-        if(!isLeaf(index))
-        { 
-            if(array.get(index).compareTo(array.get(getLeftIndex(index))) > 0 || array.get(index).compareTo(array.get(getRightIndex(index))) > 0)
-            {
-                if(array.get(getLeftIndex(index)).compareTo(array.get(getRightIndex(index))) > 0)
-                {
-                    swap(array, index, getLeftIndex(index));
-                    heapify(getLeftIndex(index));
-                }
-                else
-                {
-                    swap(array, index, getRightIndex(index));
-                    heapify(getRightIndex(index));
-                }
-            }
-        }
-    }
-
-    private boolean isLeaf(int index)
-    {
-        if (index >=  (array.size() / 2)  &&  index <= array.size()){ return true; }
-        
-        return false;
     }
 
     //inserimento di un elemento - O(log n)
     public void insert(T element)
     {
         array.add(element);
-
-        int index = array.size() - 1;
-
-        while(index > 0 && array.get(index).compareTo(array.get(getParentIndex(index))) < 0)
-        {
-            swap(array, index, getParentIndex(index));
-
-            index = getParentIndex(index);
-        }
+        
+        bubbleUp();
     }
+
+    private void bubbleUp()
+    {
+		int index = array.size() -1;
+        
+        while (hasParent(index)
+                && (getParentElement(index).compareTo(array.get(index)) > 0)) {
+            // parent/child are out of order; swap them
+            swap(array, index, getParentIndex(index));
+            index = getParentIndex(index);
+        } 
+	}
 
     //restituzione della dimensione dello heap - O(1)
     public int getSize(){ return array.size(); }
 
     //restituzione del genitore di un elemento - O(1)
-    public int getParentIndex(int i){ return ((i - 1) / 2); }
+    public int getParentIndex(int i){ return (i / 2); }
     public T getParentElement(int i){ return array.get(getParentIndex(i)); }
+    public boolean hasParent(int i){ return i > 1; }
 
     //restituzione del figlio sinistro di un elemento - O(1)
-    public int getLeftIndex(int i){ return (2 * i) + 1; }
+    public int getLeftIndex(int i){ return (2 * i); }
     public T getLeftElement(int i){ return array.get(getLeftIndex(i)); }
+    public boolean hasLeftChild(int i){ return getLeftIndex(i) < array.size() - 1;}
 
     //restituzione del figlio destro di un elemento - O(1)
-    public int getRightIndex(int i){ return ((2* i) + 2); }
+    public int getRightIndex(int i){ return (2 * i) + 1; }
     public T getRightElement(int i){ return array.get(getRightIndex(i)); }
+    public boolean hasRightChild(int i){ return getLeftIndex(i) <= array.size() - 1; }
 
     //estrazione dell'elemento con valore minimo - O(log n)
     public T extractElement()
     {
         T root = array.remove(ROOT);
         T first = array.remove(array.size() - 1);
-
+        
         array.add(ROOT, first);
         
-        heapify(array.size());
-
+        bubbleDown();
+        
         return root;
     }
 
@@ -95,32 +75,102 @@ public class Heap<T extends Comparable<T>>
 
     public void print()
     {
-        for (int i = 0; i < (this.array.size() - 1) / 2; i++)
+        System.out.println("Array size: " + array.size()+"\n");
+
+        for (int i = 0; i < (array.size()); i++)
         {         
-            System.out.println("NODE : " + array.get(i) + " LEFT CHILD : " + array.get(getLeftIndex(i)) + " RIGHT CHILD :" + array.get(getRightIndex(i)));
+            System.out.println("Array is: " + array.get(i));
         } 
+    }
+
+    private void bubbleDown()
+    {
+        int index = ROOT;
+
+        int leftChild, rightChild, minIndex;
+		T tmp;
+
+		while(true){
+			leftChild = getLeftIndex(index);
+			rightChild = getRightIndex(index);
+			minIndex = index;
+			if(array.size() > leftChild && array.get(minIndex).compareTo(array.get(leftChild)) > 0)
+				minIndex = leftChild;
+			if(array.size() > rightChild && array.get(minIndex).compareTo(array.get(rightChild)) > 0)
+				minIndex = rightChild;
+
+			if (index != minIndex){
+				tmp  = array.get(index);
+				array.set(index, array.get(minIndex));
+				array.set(minIndex, tmp);
+				index = minIndex;
+			}
+			else 
+				return;			 
+		}
     }
     public static void main(String[] args)
     {
         System.out.println("The Min Heap is ");
-        Heap<Integer> minHeap = new Heap<Integer>(10);
-        minHeap.insert(7);
-        minHeap.insert(9);
-        minHeap.insert(8);
-        minHeap.insert(3);
+        Heap<Integer> minHeap = new Heap<Integer>();
+
         minHeap.insert(5);
+        minHeap.insert(2);
+        minHeap.insert(10);
+        minHeap.insert(7);
+        minHeap.insert(12);
+        minHeap.insert(11);
+        minHeap.insert(8);
+        minHeap.insert(13);
+        minHeap.insert(3);
+        minHeap.insert(4);
+        minHeap.insert(9);
         minHeap.insert(6);
         minHeap.insert(1);
-        minHeap.insert(2);
-        minHeap.insert(4);
-        minHeap.insert(10);
+        
         minHeap.buildMinHeap();
 
-        System.out.println("The Min val is " + minHeap.extractElement());
+        System.out.println("Remove " + minHeap.extractElement()+"\n");
+        minHeap.buildMinHeap();
+
+        System.out.println("Remove " + minHeap.extractElement()+"\n");
+        minHeap.buildMinHeap();
+        
+        System.out.println("Remove " + minHeap.extractElement()+"\n");
+        minHeap.buildMinHeap();
+        
+        System.out.println("Remove " + minHeap.extractElement()+"\n");
+        minHeap.buildMinHeap();
+        
+        System.out.println("Remove " + minHeap.extractElement()+"\n");
+        minHeap.buildMinHeap();
+        
+        System.out.println("Remove " + minHeap.extractElement()+"\n");
+        minHeap.buildMinHeap();
+        
+        System.out.println("Remove " + minHeap.extractElement()+"\n");
+        minHeap.buildMinHeap();
+        
+        System.out.println("Remove " + minHeap.extractElement()+"\n");
+        minHeap.buildMinHeap();
+        
+        System.out.println("Remove " + minHeap.extractElement()+"\n");
+        minHeap.buildMinHeap();
+        
+        System.out.println("Remove " + minHeap.extractElement()+"\n");
+        minHeap.buildMinHeap();
+        
+        System.out.println("Remove " + minHeap.extractElement()+"\n");
+        minHeap.buildMinHeap();
+        
+        System.out.println("Remove " + minHeap.extractElement()+"\n");
+        minHeap.buildMinHeap();
+        
+        
 
         /*minHeap.decreaseElement(1, 55);
         
-        System.out.println("The Min val is " + minHeap.extractElement());*/
+        System.out.println("Remove " + minHeap.extractElement());*/
 
     }
 }
